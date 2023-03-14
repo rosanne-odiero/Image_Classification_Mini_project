b044a70766b78b5bd512653229f40e189b518ef6
Rosanne Odiero <rosanneodiero9@gmail.com>
2023-03-14 18:56:23 +0300

    Modified test onnx code to accommodate new model file

--- test_onnx.py
+++ test_onnx.py
@@ -1,15 +1,26 @@
 
 # test onnx
-from load_onnx import load_onnx_model
+from model import Model, Preprocessor
 
 
 def test_load_onnx_model_1():
     image_file = "n01667114_mud_turtle.jpeg"
     onnx_file = "pytorch_model_weights.onnx"
-    assert load_onnx_model(image_file, onnx_file) == ('n01667114_mud_turtle', 35)
+    
+    p = Preprocessor(image_file)
+    pimg = p.preprocess_numpy(p.load_image())
+    
+    m = Model(pimg, onnx_file)
+    assert m.load_and_predict() == ('n01667114_mud_turtle', 35)
     
     
 def test_load_onnx_model_2():
     image_file = "n01440764_tench.jpeg"
     onnx_file = "pytorch_model_weights.onnx"
-    assert load_onnx_model(image_file, onnx_file) == ('n01440764_tench', 0)
+    
+    p = Preprocessor(image_file)
+    pimg = p.preprocess_numpy(p.load_image())
+    
+    m = Model(pimg, onnx_file)
+    
+    assert m.load_and_predict() == ('n01440764_tench', 0)
